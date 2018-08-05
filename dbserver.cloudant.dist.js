51731a3a9a22f52303571cfeb13318e45b8beaa9
David Fall <davidfall@rocketmail.com>
2018-08-05 13:33:55 -0700

    PA: Don’t ignore dist configs

---
+++ dbserver.cloudant.dist.js
@@ -0,0 +1,11 @@
+var dbServerConfig =  {
+    protocol: 'https://',
+    host: 'ba135a53-f972-43ba-81d1-611c9fc3c5d3-bluemix.cloudant.com',
+    user: 'ba135a53-f972-43ba-81d1-611c9fc3c5d3-bluemix',
+    password: 'cfc8b010be3f69c4b40446172746395c5952b11370d935a0732ed81a8cc75198',
+    userDB: 'sl-users',
+    couchAuthDB: '_users',
+    cloudant: true,
+};
+
+module.exports = dbServerConfig;
---
+++ dbserver.dist.js
@@ -0,0 +1,11 @@
+var dbServerConfig = {
+	protocol: 'https://',
+	host: 'couchdb-218155.smileupps.com',
+	user: 'admin',
+	password: '9cd08fe7c703',
+	userDB: 'sl-users',
+	couchAuthDB: '_users',
+	cloudant: false,
+};
+
+module.exports = dbServerConfig;
